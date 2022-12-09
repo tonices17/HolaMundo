dffa84a66e70b8d97c10ce8d87162ee7a55f2e9f
Ruben <rubmorsan@alu.edu.gva.es>
2022-12-09 10:19:55 +0100

    version ruben

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[]args){
-        System.out.println("Hola TOnices, que taasdala estas .Mundodadadadadadaa.");
+        System.out.println("Hola TOnices, que taasdalaasdasdasd estas .Mundodadadadadadaa.");
     }
 }
